4f3d20df28bde16b1a8332ca7d71a215b5b9f3d2
douglas85rj <douglas85rj@gmail.com>
2023-07-27 11:15:10 -0300

    MétodoRetornandoObjeto

---
+++ calculadora-imc/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args) {
+        Paciente paciente = new Paciente();
+        paciente.altura = 1.82;
+        paciente.peso = 125;
+
+        IndiceMassaCorporal imc = paciente.calcularIndiceMassaCorporal();
+
+        if (imc.resultado >= 30) {
+            System.out.printf("Paciente com altura de %.2f e peso de %.2f " +
+                    "está com obesidade %n", imc.altura, imc.peso);
+        }
+
+        System.out.printf("IMC: %.2f%n", imc.resultado);
+    }
+}
