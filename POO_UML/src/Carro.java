4f7cbd22d2cdaa680e0f3f903e596e84f524db16
douglas85rj <douglas85rj@gmail.com>
2023-07-27 10:25:06 -0300

    Evitando_métodos_duplicados&Criar_MétodoCalcularIpva

--- POO_UML/src/Carro.java
+++ POO_UML/src/Carro.java
@@ -11,13 +11,25 @@ public class Carro {
         double valorRevendaMeuCarro = calcularValorRevenda();
         int tempoDeUsoMeuCarro = calcularTempoDeUsoEmAnos();
 
+        if (valorCompra <= 0) {
+            System.out.printf("Carro com preço de compra zerado. " +
+                    "Não foi foi possível imprimir resumo de depreciação.");
+            return;
+        }
+
         System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoMeuCarro);
         System.out.printf("Valor de revenda: %6.2f%n", valorRevendaMeuCarro);
     }
-    void imprimirIpva(){
+
+    void imprimirIpva() {
         System.out.printf("O valor do seu IPVA é : %6.2f%n", calcularIpva());
     }
-    double calcularIpva(){
+
+    double calcularIpva() {
+        int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
+        if (tempoDeUsoEmAnos >= 10) {
+            return 0;
+        }
         return calcularValorRevenda() * 0.04;
     }
 
