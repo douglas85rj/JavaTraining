a5a83506ab7b121623a4f755b278b1a2579fb19a
douglas85rj <douglas85rj@gmail.com>
2023-09-06 19:57:35 -0300

    forArray

--- for com array/src/ExemploForArray.java
+++ for com array/src/ExemploForArray.java
@@ -2,8 +2,11 @@ public class ExemploForArray {
     public static void main(String[] args) {
 String alunos [] = {"João", "Maria", "José", "Pedro", "Ana"};
 
-for (int x = 0; x < alunos.length; x++) {
-    System.out.println("Aluno: " + alunos[x]);
+// for (int x = 0; x < alunos.length; x++) {
+//     System.out.println("Aluno: " + alunos[x]);
+
+for (String aluno : alunos) {
+    System.out.println("Aluno: " + aluno);
 }
     }
 }
