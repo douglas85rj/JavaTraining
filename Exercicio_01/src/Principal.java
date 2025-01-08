b5dfd70106d76ad9004ee075e27ff4d3e1f3632c
douglas85rj <douglas85rj@gmail.com>
2025-01-08 12:18:07 -0300

    Finish_desafio01_Nelioalves

--- Exercicio_01/src/Principal.java
+++ Exercicio_01/src/Principal.java
@@ -5,15 +5,26 @@ public class Principal {
 		String product1 = "computer";
 		String product2 = "Office desk";
 
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
 	
 
-		double price1 = 2100.00;
+		double price1 = 2100.0;
 		double price2 = 650.50;
+		double measure = 53.234567;
 	
 
 		System.out.println("Products:");
-		System.out.printf("%s which is  %d", product1, price2);
-		System.out.printf("%s which is  %d", product2, price1);
+		System.out.println(product1 + ", which price is $ " + price1);
+		System.out.println(product2 + ", which price is $ " + price2);
+		System.out.println();
+		System.out.println("Record: " + age + " years old, " + code + " code and gender: " + gender);
+		System.out.println();
+		System.out.println("Measure with eight decimal places: " + measure);
+		System.out.printf("Rounded (three decimal places): %.3f\n", measure);
+		System.out.printf("US decimal point: %.3f\n", measure);
+		
 
 	}
 
