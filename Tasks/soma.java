792a3cabf4aab349ea68cd1d6902336e35b13670
douglas85rj <douglas85rj@gmail.com>
2025-01-09 23:20:03 -0300

    task_modulo01

---
+++ Tasks/soma.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class soma {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int soma = num1 + num2;
+
+        System.out.println("A soma dos números é: " + soma);
+
+        sc.close();
+
+    }
+    
+}
