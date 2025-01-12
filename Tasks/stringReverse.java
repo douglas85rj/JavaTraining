181bcd40ee4d342ace245046fb7d5fe2639ccba9
douglas85rj <douglas85rj@gmail.com>
2025-01-12 19:34:48 -0300

    Update_StringReverse

--- Tasks/stringReverse.java
+++ Tasks/stringReverse.java
@@ -1,17 +1,23 @@
 package Tasks;
 
 public class stringReverse {
-    
 
-        public void reverseString(char[] s) {
-            int left = 0, right = s.length - 1;
-            while (left < right) {
-                char temp = s[left];
-                s[left] = s[right];
-                s[right] = temp;
-                left++;
-                right--;
-            }
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
         }
     }
 
+    public static void main(String[] args) {
+        stringReverse reverser = new stringReverse();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        System.out.println("Original: " + new String(s));
+        reverser.reverseString(s);
+        System.out.println("Reversed: " + new String(s));
+    }
+}
