62b29b5e2d3e37188f0801e2bb34c9768d9ae59e
douglas85rj <douglas85rj@gmail.com>
2025-02-02 11:57:52 -0300

    toString

--- Poo_estoque/src/application/App.java
+++ Poo_estoque/src/application/App.java
@@ -1,6 +1,27 @@
 package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+
+       Product product = new Product();
+       System.out.println("Enter product data:");
+       System.out.println("Name:");
+       product.name = sc.nextLine();
+
+       System.out.println("Price: ");
+        product.price = sc.nextDouble();
+
+       System.out.println("Quantity in stock:");
+       product.quantity = sc.nextInt();
+
+       System.out.println(product);
+       sc.close();
     }
 }
--- Poo_estoque/src/entities/Product.java
+++ Poo_estoque/src/entities/Product.java
@@ -18,4 +18,15 @@ public class Product {
     this.quantity -= quantity;
     }
 
+    public String toString(){
+        return name
+        +", $ "
+        + String.format("%.2f", price)
+        +", $ "
+        +quantity
+        + " units, Total: $ "
+        + String.format("%.2f", totalValueInStock()); 
+        
+    }
+
 }
